7f0f723f7db4d97254336da2170340ef2d0cd80a
HelenVirtanen <124844232+HelenVirtanen@users.noreply.github.com>
2025-02-14 01:32:27 +0300

    Create likes.js

---
+++ js/likes.js
@@ -0,0 +1,13 @@
+let heart = document.querySelector('.heart');
+let heartView = document.querySelector('.heart i.fa-heart');
+let likesNumber = document.querySelector('.likes-number');
+
+heart.onclick = function () {
+  if (heart.classList.contains('added')) {
+    likesNumber.textContent--;
+  } else {
+    likesNumber.textContent++;
+  }
+  heart.classList.toggle('added');
+  heartView.classList.toggle('fa-regular');
+};
